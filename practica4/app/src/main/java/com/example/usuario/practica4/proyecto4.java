96cba5b6852815de0d2a2c4c97bd2ec90cf9cd4b
Bryan Peña <bryan07182@hotmail.com>
2017-08-29 08:45:35 -0600

    se empieza a usar la practica 4

--- practica4/app/src/main/java/com/example/usuario/practica4/proyecto4.java
+++ practica4/app/src/main/java/com/example/usuario/practica4/proyecto4.java
@@ -2,11 +2,14 @@ package com.example.usuario.practica4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class proyecto4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toast toast = Toast.makeText(this,"Cadena", Toast.LENGTH_LONG);
+        toast.show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proyecto4);
     }
