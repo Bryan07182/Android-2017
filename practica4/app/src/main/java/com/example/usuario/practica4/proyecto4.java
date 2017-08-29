4ad5b27548991d85b0f16114f9be9020c9a7537e
Bryan Peña <bryan07182@hotmail.com>
2017-08-29 08:19:02 -0600

    empieza practica 4

---
+++ practica4/app/src/main/java/com/example/usuario/practica4/proyecto4.java
@@ -0,0 +1,13 @@
+package com.example.usuario.practica4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class proyecto4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proyecto4);
+    }
+}
